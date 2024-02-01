d9388521c3595fd8065c778de4d9017b42c49d22
Nekroid <philipleinung@aol.de>
2024-02-01 10:56:37 +0100

    Bird ergänzungen ++

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,6 +4,7 @@ import com.cc.java.bees.Drone;
 import com.cc.java.bees.HoneyBee;
 import com.cc.java.bees.Queen;
 import com.cc.java.bees.Worker;
+import com.cc.java.birds.Bird;
 
 public class App {
     
@@ -12,24 +13,33 @@ public class App {
        Queen queen = new Queen();
        Worker worker = new Worker();
        Drone drone = new Drone();
+       Bird bird = new Bird();
        
        pollObj(queen);
        pollObj(worker);
        pollObj(drone);
 
-        //-- Das geht nicht mehr - ABSTRAKTION!--//
-        // HoneyBee bee = new HoneyBee();
-        // pollObj(bee);
+
+       pollObj(bird);
+
 
     }
 
-    // ---- 3. Iteration Polymorphie -----//
-    private static void pollObj(HoneyBee obj){
+    // ----Polymorphie Bees-----//
+    private static void pollObj(HoneyBee obj)
+    {
         output(obj.doYourJob());
         output(obj.fly());
         output("-----------------");
     }
 
+    // ----überladene Methode Bird(s)-----//
+    private static void pollObj(Bird obj)
+    {
+        output(obj.hasFeathers());
+        output(obj.fly());
+        output("-----------------");
+    }
 
     private static void output(String outputStr) {
         System.out.println(outputStr);
